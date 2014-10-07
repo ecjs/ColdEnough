d2be6a94f48bf16340404a862f99ea7fea607915
Mark Harrell <mark@mjharrell.com>
2014-10-07 16:41:23 -0700

    Edit app.js for env port

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+//   next();
+// });
 app.set('views','views/');
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,6 +42,6 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
   console.log("Listening on port 3000");
 });
