40b9ca47e5a7ceeb84158a78b36d9795f2569a23
Mark Harrell <mark@mjharrell.com>
2014-10-07 17:42:04 -0700

    cors added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var compress = require('compression');
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+ 
 app.use(compress());
 
 app.use(express.static(__dirname + '/public'));
