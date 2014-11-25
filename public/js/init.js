0f7060714c603643b57b0122444ddffb32877e1e
Mark Harrell <mark@mjharrell.com>
2014-11-25 11:42:45 -0800

    adding bowerify

--- public/js/init.js
+++ public/js/init.js
@@ -1,42 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#yes").on("click", function(){
+  $("#yes").on("click", function(event) {
+    event.preventDefault();
+    $("#introcold").hide();
+    cl.show();
     getLocation();
   });
 
-  $("#formsubmit").on("click", function(){
+  $("#formsubmit").on("click", function() {
     $("#geolocate").html("");
     $("#form").fadeOut();
-    getTemp( $("#city").val(), $("#state").val() );
+    getTemp($("#city").val(), $("#state").val());
     $("#geolocate").fadeIn();
   });
 
-    $( "#city" ).autocomplete({
-      source: function( request, response ) {
+  $('#city').autocomplete({
+      source: function(request, response) {
         $.ajax({
           url: "http://gd.geobytes.com/AutoCompleteCity",
           dataType: "jsonp",
           data: {
             q: request.term
           },
-          success: function( data ) {
-            response( data );
+          success: function(data) {
+            response(data);
           }
         });
       },
       minLength: 3,
-      select: function(event,ui){
+      select: function(event, ui) {
+        $("#introcold").hide();
+        cl.show();
         $.ajax({
 url: "http://gd.geobytes.com/GetCityDetails",
 dataType: "jsonp",
 data: {
   fqcn: ui.item.label
 },
-success: function( data ) {
-  $("#geolocate").html("");
-  $("#form").fadeOut();
-  getTemp(data.geobytescity,data.geobytescode);
-  $("#geolocate").fadeIn();
+success: function(data) {
+  cl.hide();
+  getTemp(data.geobytescity, data.geobytescode);
 }
         });
       }
@@ -44,8 +47,8 @@ success: function( data ) {
 
 });
 
-function getLocation(){
-  if(navigator.geolocation){
+function getLocation() {
+  if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showLocation);
   }
   else{
@@ -53,44 +56,47 @@ function getLocation(){
   }
 
 }
-function showLocation(p){
+function showLocation(p) {
   $.ajax({
-    url: 'http://localhost:3000/zip',
+    url: '/zip',
     type: 'POST',
     cache: 'FALSE',
     data: { lat: p.coords.latitude, long: p.coords.longitude},
-    complete: function(){
+    complete: function() {
       console.log('process complete');
     },
-    success: function(data){
-      getTemp(data.city,data.state);
+    success: function(data) {
+      getTemp(data.city, data.state);
       console.log("process success");
     },
-    error: function(){
-      console.log("process error");
+    error: function(error) {
+      console.log("process error" + error);
     }
   });
 }
 
-function getTemp(c,s){
+function getTemp(c,s) {
   $.ajax({
-    url: 'http://localhost:3000/temperature',
+    url: '/temperature',
     type: 'POST',
     cache: 'FALSE',
     data: { city: c, state: s},
-    complete: function(){
+    complete: function() {
       console.log('process complete');
     },
-    success: function(data){
-      if (data.temp <= 50){
+    success: function(data) {
+      cl.hide();
+      if (data.temp <= 50) {
         $("#introcold").html("Yes! Its currently " + data.temp + "F!  Go chill!");
+        $("#introcold").fadeIn();
       }
-      else{
+      else {
         $("#introcold").html("No, the current temp is " + data.temp + "F, it needs to be at least 50F!");
+        $("#introcold").fadeIn();
       }
     },
-    error: function(){
-      console.log("process error");
+    error: function(error) {
+      console.log("process error" + error);
     }
   });
 }
